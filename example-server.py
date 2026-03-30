918abff6be3e2822924991981060f2a3efb1be11
austinjp <austin.plunkett@gmail.com>
2026-03-30 17:41:14 +0100

    Tweak example. Quit early if port not available.

--- example-server.py
+++ example-server.py
@@ -1,24 +1,31 @@
 #!/usr/bin/env python3
 
-
-print("Loading models...", flush=True)
-import en_core_web_lg as model
-import coreferee
-
-
-# Defaults for en_core_web_md = [tok2vec tagger parser senter attribute_ruler lemmatizer ner sentencizer]
-# Reminder: disabling 'parser' seems to cause seg faults :/
-# TODO:
-# Use nlp.pipe_names to prompt user about which components to enable/disable?
-
-nlp = model.load()
-nlp.add_pipe("coreferee")
-
-
-from spacy_service.server import serve
-
+host = "localhost"
 port = 50051
 max_workers = 10
 
+
+# Check port is available:
+import socket
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    if sock.connect_ex((host, port)) == 0:
+        print(f"Port {port} already in use! Quitting!")
+        exit(1)
+
+
+# Load spaCy model.
+print("Loading spacy...", flush=True)
+import en_core_web_lg as model
+nlp = model.load()
+
+
+# Add coreferencing.
+print("Loading coreferee...", flush=True)
+import coreferee
+nlp.add_pipe("coreferee")
+
+
+# Start the server.
+from spacy_service.server import serve
 print("Starting service on port", port, flush=True)
 serve(nlp=nlp, port=port, max_workers=max_workers)
