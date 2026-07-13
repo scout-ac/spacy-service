462ab1ba43a68e3b850235af4c1a3f1ebaff2f0e
austinjp <austin.plunkett@gmail.com>
2026-07-13 16:36:18 +0100

    Add json_name field annotations. Strip Go-specific field annotations when generating Python code.

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -11,6 +11,13 @@ cd "${wd}" &>/dev/null
 
 echo "Generating GRPC code for Python..."
 
+# Rename original file to temp file.
+t=$(mktemp -p ./proto -t XXXXXXXX.proto)
+mv proto/spacy_service.proto "${t}"
+
+# Strip Go-specific fields, producing Python-specific version of original.
+sed -re 's|,\s? \(go.field\)\.name[^]]+||g' "${t}" > proto/spacy_service.proto
+
 while read line ; do
     echo "    ${line}" | sed -re 's,Writing ,Writing '"${dest}"',g'
 done < <(
@@ -20,21 +27,37 @@ done < <(
         --python_betterproto_opt=typing.310 \
         --python_betterproto_out="${dest}" \
         --grpc_python_out="${dest}" \
-        proto/*.proto \
+        proto/spacy_service.proto \
         2>&1
 )
+
+# Restore original.
+mv -f "${t}" proto/spacy_service.proto
 
 ################################################################################
 # Yuck. Hacks follow.
 ################################################################################
 
-pushd spacy_service/generated &>/dev/null
+pushd "${dest}" &>/dev/null
 
 # Ensure filename compatability with protoc defaults.
+if [[ ! -r spacy_service.py ]]; then
+	echo "Could not find file spacy_service.py, quitting!" >&2
+	exit 1
+fi
+
 echo "Renaming: spacy_service.py  ->  spacy_service_pb2.py"
 mv spacy_service.py spacy_service_pb2.py
 
+
+if [[ ! -r spacy_service_pb2_grpc.py ]]; then
+	echo "Could not find file spacy_service_pb2_grpc.py, quitting!" >&2
+	exit 1
+fi
+
 # See https://github.com/grpc/grpc/issues/29459
-sed -i'' -r -e 's/^import spacy_service/from spacy_service.generated import spacy_service/g' *pb2_grpc.py
+echo "Tweaking import."
+sed -i'' -r -e 's/^import spacy_service/from spacy_service.generated import spacy_service/g' spacy_service_pb2_grpc.py
 
 popd &>/dev/null
+echo "Done!"
